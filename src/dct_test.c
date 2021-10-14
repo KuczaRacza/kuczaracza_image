471631023daaafd25669cc72b100bc5b261bf6e1
KuczaRacza <kuczaracza31@gmail.com>
2021-10-14 16:49:56 +0200

    dct still doesn't  work

--- src/dct_test.c
+++ src/dct_test.c
@@ -6,55 +6,47 @@
 #include <math.h>
 #include <stdio.h>
 #include <string.h>
-void dct(float **DCTMatrix, float **Matrix, int N, int M){
+void idct(float **Matrix, float **DCTMatrix, int N, int M) {
+	int i, j, u, v;
 
-    int i, j, u, v;
-    for (u = 0; u < N; ++u) {
-        for (v = 0; v < M; ++v) {
-        DCTMatrix[u][v] = 0;
-            for (i = 0; i < N; i++) {
-                for (j = 0; j < M; j++) {
-                    DCTMatrix[u][v] += Matrix[i][j] * cos(M_PI/((float)N)*(i+1./2.)*u)*cos(M_PI/((float)M)*(j+1./2.)*v);
-                }               
-            }
-        }
-    }  
- }
+	for (u = 0; u < N; ++u) {
+		for (v = 0; v < M; ++v) {
+			Matrix[u][v] = 1 / 4. * DCTMatrix[0][0];
+			for (i = 1; i < N; i++) {
+				Matrix[u][v] += 1 / 2. * DCTMatrix[i][0];
+			}
+			for (j = 1; j < M; j++) {
+				Matrix[u][v] += 1 / 2. * DCTMatrix[0][j];
+			}
 
-void idct(float **Matrix, float **DCTMatrix, int N, int M){
-    int i, j, u, v;
+			for (i = 1; i < N; i++) {
+				for (j = 1; j < M; j++) {
+					Matrix[u][v] += DCTMatrix[i][j] * cos(M_PI / ((float)N) * (u + 1. / 2.) * i) * cos(M_PI / ((float)M) * (v + 1. / 2.) * j);
+				}
+			}
+			Matrix[u][v] *= 2. / ((float)N) * 2. / ((float)M);
+		}
+	}
+}
+void dct(float **DCTMatrix, float **Matrix, int N, int M) {
 
-    for (u = 0; u < N; ++u) {
-        for (v = 0; v < M; ++v) {
-          Matrix[u][v] = 1/4.*DCTMatrix[0][0];
-          for(i = 1; i < N; i++){
-          Matrix[u][v] += 1/2.*DCTMatrix[i][0];
-           }
-           for(j = 1; j < M; j++){
-          Matrix[u][v] += 1/2.*DCTMatrix[0][j];
-           }
-
-           for (i = 1; i < N; i++) {
-                for (j = 1; j < M; j++) {
-                    Matrix[u][v] += DCTMatrix[i][j] * cos(M_PI/((float)N)*(u+1./2.)*i)*cos(M_PI/((float)M)*(v+1./2.)*j);
-                }               
-            }
-        Matrix[u][v] *= 2./((float)N)*2./((float)M);
-        }
-    }  
- }
-
-
+	int i, j, u, v;
+	for (u = 0; u < N; ++u) {
+		for (v = 0; v < M; ++v) {
+			DCTMatrix[u][v] = 0;
+			for (i = 0; i < N; i++) {
+				for (j = 0; j < M; j++) {
+					DCTMatrix[u][v] += Matrix[i][j] * cos(M_PI / ((float)N) * (i + 1. / 2.) * u) * cos(M_PI / ((float)M) * (j + 1. / 2.) * v);
+				}
+			}
+		}
+	}
+}
 bitmap *prepare_image(float **matrix, u32 size_x, u32 size_y) {
-	bitmap *b = create_bitmap(size_x, size_y, size_x , RGB24);
+	bitmap *b = create_bitmap(size_x, size_y, size_x * 3, RGB24);
 	for (u32 i = 0; i < size_x; i++) {
 		for (u32 j = 0; j < size_y; j++) {
-			float mat = matrix[i][j] ;
-			if (mat > 255.0f) {
-				mat = 255.0f;
-			} else if (mat < 0.0f) {
-				mat = 0.0f;
-			}
+			float mat = matrix[i][j];
 			union piexl_data px;
 			for (u32 c = 0; c < 3; c++) {
 				px.channels[c] = mat;
@@ -64,36 +56,47 @@ bitmap *prepare_image(float **matrix, u32 size_x, u32 size_y) {
 	}
 	return b;
 }
-float **preapare_matix(bitmap *b) {
+float **preapare_matix(bitmap *b, u32 dimX, u32 dimY) {
 
-	float **matrix = (float **)malloc(sizeof(float *) * b->x);
-	for (u32 i = 0; i < b->x; i++) {
-		matrix[i] = malloc(sizeof(float) * b->y);
-		for (u32 j = 0; j < b->y; j++) {
-			union piexl_data color_ptr;
-			u32 col = 0;
-			color_ptr.pixel = get_pixel(i, j, b);
-			for (u32 c = 0; c < 3; c++) {
-				col += color_ptr.channels[c];
+	float **m = calloc(dimX, sizeof(float *));
+	float *p = calloc(dimX * dimY, sizeof(float));
+	int i;
+	for (i = 0; i < dimX; i++) {
+		m[i] = &p[i * dimY];
+	}
+	if (b != NULL) {
+
+		for (u32 i = 0; i < b->x; i++) {
+			for (u32 j = 0; j < b->y; j++) {
+				union piexl_data color;
+				color.pixel = get_pixel(i, j, b);
+				m[i][j] = color.rgba.r;
 			}
-			matrix[i][j] =  color_ptr.channels[0];
 		}
 	}
-	return matrix;
+	return m;
+}
+void write_mat(FILE *f, float **mat, u32 x, u32 y) {
+
+	for (u32 i = 0; i < y; i++) {
+		for (u32 j = 0; j < x; j++) {
+			fprintf(f, "%f\t", mat[j][i]);
+		}
+		fprintf(f, "\n");
+	}
 }
 int main(int argc, char **argv) {
+	FILE * f = fopen("mat.txt", "w");
 	SDL_Surface *img = IMG_Load(argv[1]);
 	bitmap *b_r = sdl_to_bitmap(img);
-	float **matrix = preapare_matix(b_r);
-	float **DCT_matrix = malloc(b_r->y * sizeof(float *));
-	for (u32 i = 0; i < b_r->y; i++) {
-		DCT_matrix[i] = calloc(sizeof(float), b_r->x);
-	}
+	float **matrix = preapare_matix(b_r, b_r->x, b_r->y);
+	write_mat(f, matrix, b_r->x, b_r->y);
+	float **DCT_matrix = preapare_matix(NULL, b_r->x, b_r->y);
 	dct(DCT_matrix, matrix, b_r->x, b_r->y);
-	for (u32 i = 0; i < b_r->y; i++) {
-		memset(matrix[i], 0, b_r->x * sizeof(float));
-	}
+	free(matrix);
+	matrix = preapare_matix(NULL, b_r->x, b_r->y);
 	idct(matrix, DCT_matrix, b_r->x, b_r->y);
+	write_mat(f, matrix, b_r->x, b_r->y);
 	bitmap *b = prepare_image(matrix, img->w, img->h);
 	SDL_Surface *srf = bitmap_to_sdl(b);
 	SDL_SaveBMP(srf, "aaaaa.bmp");
